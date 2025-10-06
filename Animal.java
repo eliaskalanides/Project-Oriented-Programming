16cc1c58357b05aa924921a418665285d7e3aad7
eliaskalanides <elias.kalanidis@gmail.com>
2025-10-06 21:20:58 +0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,18 @@
+package Dog;
+
+public class Animal 
+{
+	String name ;
+	int numOfLegs;
+	boolean isHerbivore;
+	boolean isAsleep;
+	boolean canFly;
+	String toy[] = {"ball","rope","teddybear"};
+	Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
---
+++ TestAnimal.java
@@ -0,0 +1,18 @@
+package Dog;
+
+public class TestAnimal 
+{
+
+	public static void main(String[] args) 
+	{
+		Animal Cow = new Animal();
+		
+		Cow.numOfLegs(4);
+		Cow.isHerbivore(false);
+		Cow.isAsleep(false);
+		Cow.canFly(false)
+		Cow.toy(rope);
+		
+	}
+
+}
