90b366fb1a2aaea0a78b1ea6c92fe59371e041b4
eliaskalanides <elias.kalanidis@gmail.com>
2025-10-06 20:02:36 +0300

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,17 @@
+package Looping;
+
+import java.util.Scanner;
+
+public class Looping 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		for(int i=1 ; i<=10;i++)
+		
+		System.out.println(i);
+		
+	}
+
+}
