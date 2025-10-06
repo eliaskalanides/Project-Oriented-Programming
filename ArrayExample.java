be29a42b1db5e669f905dd1b56d5c817355b8427
eliaskalanides <elias.kalanidis@gmail.com>
2025-10-06 20:08:30 +0300

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,23 @@
+package ArrayExample;
+
+import java.util.Scanner;
+
+public class ArrayExample 
+{
+    public static void main(String[] args) 
+    {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Average: " + average);
+    }
+}
+
